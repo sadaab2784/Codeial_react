6b78e03457c9e8a89ac6f2b05c79dc68eea5eab4
sadaab2784 <ali.sadaab825313@gmail.com>
2022-10-08 12:34:44 +0530

    Adding navbar

--- src/components/App.js
+++ src/components/App.js
@@ -14,6 +14,57 @@ class App extends React.Component {
     const { posts } = this.props;
     return (
       <div>
+        <nav className="nav">
+          <div className="left-div">
+            <img
+              src="https://cdn-icons-png.flaticon.com/800/7413/7413126.png"
+              alt="logo"
+            />
+          </div>
+          <div className="search-container">
+            <img
+              className="search-icon"
+              src="https://cdn-icons-png.flaticon.com/512/54/54481.png"
+              alt="search-icon"
+            />
+            <input placeholder="Search" />
+            <div className="search-results">
+              <ul>
+                <li className="search-result-row">
+                  <img
+                    src="https://cdn-icons-png.flaticon.com/512/2202/2202112.png"
+                    alt="user-dp"
+                  />
+                  <span>John cena</span>
+                </li>
+                <li className="search-result-row">
+                  <img
+                    src="https://cdn-icons-png.flaticon.com/512/2202/2202112.png"
+                    alt="user-dp"
+                  />
+                  <span>John Doe</span>
+                </li>
+              </ul>
+            </div>
+          </div>
+          <div className="right-nav">
+            <div className="user">
+              <img
+                src="https://cdn-icons-png.flaticon.com/512/2202/2202112.png"
+                alt="user-dp"
+                id="user-dp"
+              />
+              <span>John Doe</span>
+            </div>
+            <div className="nav-links">
+              <ul>
+                <li>Log in</li>
+                <li>Log out</li>
+                <li>Register</li>
+              </ul>
+            </div>
+          </div>
+        </nav>
         <PostsList posts={posts} />
       </div>
     );
